3ed4d6a2632e8ba26050069f1d38f30d25f83490
Abdel-Khafid kolawole Fifa SALAOU <119133987+adeola003@users.noreply.github.com>
2023-05-01 19:19:20 +0100

    to do list features

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
+import Header from './components/header';
+import TodosLogic from './components/TodosLogic';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          To do list App
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <TodosLogic />
+    </>
+    
   );
-}
+
+};
 
 export default App;
