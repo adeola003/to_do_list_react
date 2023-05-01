885542467ea2e2dc8b93139dc3581b572506249f
Abdel-Khafid kolawole Fifa SALAOU <119133987+adeola003@users.noreply.github.com>
2023-05-01 12:32:19 +0100

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          To do list App
         </a>
       </header>
     </div>
